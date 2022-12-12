a531a96987a526d9fa19e50cb9622f9d8be34d06
KoenZhang <2823474269@qq.com>
2022-12-12 21:11:17 +0800

    cnblog美化文件

---
+++ foot.js
@@ -0,0 +1,44 @@
+<script>
+  window.$silence = {
+    avatar: 'https://images.cnblogs.com/cnblogs_com/blogs/778268/galleries/2253962/o_221211122814_%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20221211202504.jpg',
+    favicon: 'https://files.cnblogs.com/files/blogs/778268/icon.ico',
+    github: 'https://github.com/KoenZhang',
+    defaultMode: 'auto',
+    defaultTheme: 'a',
+    navbars: [{
+      title: '标签',
+      url: 'https://www.cnblogs.com/zkoen/tag/'
+    }, {
+      title: '归档',
+      url: 'https://www.cnblogs.com/zkoen/p/'
+    }, {
+      title: '开源',
+      children: [{
+        title: 'Silence',
+        target: '_blank',
+        url: 'https://github.com/KoenZhang',
+      }, {
+        title: 'Dddify',
+        target: '_blank',
+        url: 'https://github.com/KoenZhang',
+      }]
+    }],
+    catalog: {
+      enable: true,
+      index: true,
+      active: false,
+      levels: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
+    },
+    signature: {
+      enable: true,
+      author: 'YSWLHS',
+      license: ['署名-非商业性使用-相同方式共享 4.0 国际', 'https://creativecommons.org/licenses/by-nc-sa/4.0/'],
+    },
+    // sponsor: {
+    //   enable: false,
+    //   wechat: 'https://images.cnblogs.com/cnblogs_com/esofar/972540/o_wechat.png',
+    //   alipay: 'https://images.cnblogs.com/cnblogs_com/esofar/972540/o_alipay.png'
+    // }
+  };
+</script>
+<script src="https://files.cnblogs.com/files/blogs/778268/silence.self.min.js"></script>
